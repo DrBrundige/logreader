c3a554995d4e1b8598eccfa79292f3f1bca7a15d
TB Jones <brundigejones@yahoo.com>
2019-12-18 14:27:04 -0600

    Fix up dbparser
    Further documents methods in dbparser, protecting them with try blocks
    New method - run_report() runs get_range, stack_clients and write clients in succession

--- src/dbparser.py
+++ src/dbparser.py
@@ -3,79 +3,124 @@ from datetime import datetime, timedelta, time
 from src.mysqlconnection import connectToMySQL
 import csv
 
+print("Database Parser Copyright T. Brundige Jones 2019")
+
+
+# A number of methods that interact with the computer lab database including exporting data to CSV
 
 # Returns an immutable multidict of timecard objects
+# Accepts start and end times as datetime objects or strings
 def get_range(start_time, end_time):
-	mysql = connectToMySQL('computer_lab_log')
-	query = "SELECT timeclocks.id, users.first_name, users.last_name, timein, timeout, span, purposes.purpose_name\n"
-	query += "FROM computer_lab_log.timeclocks\n"
-	query += "JOIN users ON timeclocks.users_id = users.id\n"
-	query += "JOIN purposes ON timeclocks.purposes_id = purposes.id\n"
-	query += "WHERE timein >= %(i)s AND timeout < %(o)s\n"
-	query += "ORDER BY timein"
+	try:
+		mysql = connectToMySQL('computer_lab_log')
+		query = "SELECT timeclocks.id, users.first_name, users.last_name, timein, timeout, span, purposes.purpose_name\n"
+		query += "FROM computer_lab_log.timeclocks\n"
+		query += "JOIN users ON timeclocks.users_id = users.id\n"
+		query += "JOIN purposes ON timeclocks.purposes_id = purposes.id\n"
+		query += "WHERE timein >= %(i)s AND timeout < %(o)s\n"
+		query += "ORDER BY timein"
 
-	data = {
-		"i": start_time,
-		"o": end_time,
-	}
-	timecards = mysql.query_db(query, data)
-	return timecards
+		data = {
+			"i": start_time,
+			"o": end_time,
+		}
+		timecards = mysql.query_db(query, data)
+	except Exception as e:
+		print("Errant operation getting range!")
+		print(e)
+		return {}
+	else:
+		print(f"Success! {len(timecards)} rows returned!")
+		return timecards
 
 
 # Returns total span of all rows as an integer
 # Accepts immutable multidict provided by get_range (required) and purpose string.
 #   This is used as a filter
 def get_total_time(timecards, purpose=""):
-	total_span = 0
-	for row in timecards:
-		if purpose == "" or purpose == row["purpose_name"]:
-			total_span += row["span"]
-	return total_span
+	try:
+		total_span = 0
+		for row in timecards:
+			if purpose == "" or purpose == row["purpose_name"]:
+				total_span += row["span"]
+
+	except Exception as e:
+		print("Errant operation summing time!")
+		print(e)
+		return -1
+	else:
+		return total_span
 
 
 # Returns dictionary of unique clients and total time span
-# Accepts immutable multidict provided by get_range (required) and purpose string.
+# Accepts immutable multidict provided by get_range (required) and optional purpose string.
 #   This is used as a filter
 def stack_clients(timecards, purpose=""):
-	all_names = {}
-	for row in timecards:
-		if purpose == "" or purpose == row["purpose_name"]:
-			name = row["first_name"] + " " + row["last_name"]
-			if name not in all_names:
-				all_names[name] = row["span"]
-			else:
-				all_names[name] = all_names[name] + row["span"]
+	try:
+		all_names = {}
+		for row in timecards:
+			# if purpose is set to default, continues
+			# Otherwise, filters all clients where purpose is not equal to parameterized value
+			if purpose == "" or purpose == row["purpose_name"]:
+				name = row["first_name"] + " " + row["last_name"]
+				if name not in all_names:
+					all_names[name] = row["span"]
+				else:
+					all_names[name] = all_names[name] + row["span"]
+	except Exception as e:
+		print("Errant operation stacking clients!")
+		print(e)
+		return {}
+	else:
+		print(f"Success! Found {len(all_names)} unique names!")
+		return all_names
 
-	return all_names
 
-
+# Accepts dictionary returned from stack_clients
+# Outputs clients to a CSV file
+# Returns boolean based on whether the operation succeeded
 def write_stacked_clients(clients):
-	output = ["Name, Total Time"]
-	filename = "out" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".csv"
-	if __name__ != '__main__':
-		filename = 'src/' + filename
+	try:
+		assert isinstance(clients, dict), "Errant input! Clients must be a dictionary!"
+	except AssertionError as e:
+		print(e)
+		return False
 
-	with open(filename, 'w', newline='') as csvfile:
-		print("Preparing to write")
-		writer = csv.writer(csvfile, delimiter=',')
-		writer.writerow(["Name", "Total seconds", "Total time"])
-		for key, value in clients.items():
-			writer.writerow([key, value, (value / 3600 / 24)])
-	return True
+	try:
+		filename = "out" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".csv"
+		if __name__ != '__main__':
+			filename = 'src/' + filename
+
+		with open(filename, 'w', newline='') as csvfile:
+			print("Preparing to write")
+			writer = csv.writer(csvfile, delimiter=',')
+			writer.writerow(["Name", "Total seconds", "Total time"])
+			for key, value in clients.items():
+				writer.writerow([key, value, (value / 3600 / 24)])
+
+	except Exception as e:
+		print("Errant operation writing stacked clients!")
+		print(e)
+		return False
+	else:
+		print(f"Success! Clients written to file {filename}")
+		return True
+
+
+def run_report(start="", end=""):
+	if start == "" and end == "":
+		start = datetime.today()
+		start = start - timedelta(hours=start.hour)
+		start = start - timedelta(minutes=start.minute)
+		start = start - timedelta(seconds=start.second)
+		start = start - timedelta(microseconds=start.microsecond)
+		end = start + timedelta(hours=24)
+		start = start - timedelta(hours=24)
+
+	timecards = get_range(start, end)
+	clients = stack_clients(timecards)
+	print(write_stacked_clients(clients))
 
 
 if __name__ == '__main__':
-	start = datetime.today()
-	start = start - timedelta(hours=start.hour)
-	start = start - timedelta(minutes=start.minute)
-	start = start - timedelta(seconds=start.second)
-	start = start - timedelta(microseconds=start.microsecond)
-	end = start + timedelta(hours=24)
-	start = start - timedelta(hours=24)
-	timecards = get_range(start, end)
-	# print(get_total_time(timecards))
-	# print(get_total_time(timecards, "GED"))
-	# print(stack_clients(timecards))
-	# print(stack_clients(timecards, "GED"))
-	clients = stack_clients(timecards)
-	print(write_stacked_clients(clients))
+	run_report()
